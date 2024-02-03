a25052784329f911043ef51e0c45819ec19c66f8
reyhanwiyasa <reyhanwiyasa@gmail.com>
2024-02-03 08:01:59 +0700

    commit ProductRepository.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
-public class Product{
+public class Product {
     private String productId;
     private String productName;
     private int productQuantity;
